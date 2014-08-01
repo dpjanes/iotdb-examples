0d42ad6b2d1bbd5b8442ffb55a82bb814780dcf6
David P. Janes <davidjanes@davidjanes.com>
2014-08-01 16:35:24 -0400

    Made this V2

--- arduino/FirmataDimmerLight.js
+++ arduino/FirmataDimmerLight.js
@@ -11,49 +11,25 @@
 
 "use strict";
 
-var assert = require("assert")
 var iotdb = require("iotdb")
+var iot = iotdb.iot()
 
-var iot = new iotdb.IOT({
-    load_models: true,
-    load_drivers: true,
-    iotdb_device_get: false,
-    iotdb_device_create: false,
-    discover: false,
-    models_path: [
-        "$IOTDB_PROJECT/../iotdb-models/firmata"
-    ]
-});
-
-iot.on_register_things(function() {
-    var arduino_tty = iot.cfg_get("arduino_tty")
-    assert.ok(arduino_tty && arduino_tty.length)
-
-    iot.discover({
-        model: "FirmataLightDimmer",
-        driver: ":firmata",
-        initd : {
-            api: arduino_tty,
-            pin: 5
-        }
-    })
-    iot.discover({
-        model: "FirmataLightDimmer",
-        driver: ":firmata",
-        initd : {
-            api: arduino_tty,
-            pin: 3
-        }
-    })
+iot.connect({
+    model: "FirmataLightDimmer",
+    pin: 5
+})
+iot.connect({
+    model: "FirmataLightDimmer",
+    pin: 3
 })
 
-iot.on_thing(function(iot, thing) {
-    var value = 0
-    setInterval(function() {
-        thing.set('brightness', value)
-        value += 0.02
-        if (value > 1) {
-            value = 0
-        }
-    }, 100)
-})
+var dimmers = iot.things().with_model("FirmataLightDimmer")
+
+var value = 0
+setInterval(function() {
+    dimmers.set('brightness', value)
+    value += 0.02
+    if (value > 1) {
+        value = 0
+    }
+}, 50)
